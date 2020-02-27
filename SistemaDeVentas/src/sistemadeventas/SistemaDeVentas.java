e6b1230525cb92d80e91cdb34b0e12f6ef726933
ElizabethPeredo <61327189+ElizabethPeredo@users.noreply.github.com>
2020-02-27 18:26:43 +0100

    Seguna Version

--- SistemaDeVentas/src/sistemadeventas/SistemaDeVentas.java
+++ SistemaDeVentas/src/sistemadeventas/SistemaDeVentas.java
@@ -17,7 +17,7 @@ public class SistemaDeVentas {
     public static void main(String[] args) {
         // TODO code application logic here
         int x=0;
-        int y=3;
+        int y=5;
         int suma=0;
         
         
